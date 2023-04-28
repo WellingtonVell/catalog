1dc8bd23792c57e9198722c6c9849941bcb4b69d
Wellington Costa <wellingtonvell@gmail.com>
2023-04-28 14:41:07 -0300

    Co-authored-by: L. <jacksxx@users.noreply.github.com>

---
+++ src/components/navBar.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default function NavBar() {
+  return (       
+    <div>
+      <header>
+        <h1>Nome da Loja</h1>        
+      </header>   
+      <h6></h6> 
+    </div>
+  );
+}
